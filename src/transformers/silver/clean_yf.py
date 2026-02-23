16c8af38549950cfbde36a80cb169ea0498f35cd
YanisOuld <yanis.ould-mahammed@polymtl.ca>
2026-02-22 23:41:13 -0500

    feat: silver init

--- src/transformers/silver/clean_yf.py
+++ src/transformers/silver/clean_yf.py
@@ -10,7 +10,6 @@ import gzip
 from dotenv import load_dotenv
 from typing import List
 import polars as pl
-import pandas as pd
 
 load_dotenv()
 
@@ -35,7 +34,7 @@ def fetch_json_from_bronze(bucket: str, key: str) -> dict:
 
 	return json.loads(raw.decode("utf-8"))
 
-def normalize_yf(raw: dict) -> List[dict]:
+def normalize_json(raw: dict) -> List[dict]:
 	meta = raw["meta"]
 	params = meta["params"] if meta and "params" in meta else None
 	symbol = meta["partitions"]["symbol"] if meta and "partitions" in meta and "symbol" in meta["partitions"] else None
@@ -44,11 +43,10 @@ def normalize_yf(raw: dict) -> List[dict]:
 		raise ValueError("We are missing dates and symbols in the data ")
 	
 	payload = raw["payload"]
-
-	[bars] = payload
+	print(payload)
 
 	out = []
-	for bar in bars:
+	for bar in payload:
 		out.append({
 			"symbol" : symbol,
 			"ts": bar["Date"],
@@ -57,10 +55,19 @@ def normalize_yf(raw: dict) -> List[dict]:
 			"Low": bar["Low"],
 			"close": bar["Close"],
 			"volume": bar["Volume"],
-			# There is some
+			"dividends": bar["Dividends"],
+			"stock_split": bar["Stock Splits"]
+			# There is some fuck it
 		})
 
 	return out
+
+def clean_bronze(raw: dict) -> pl.DataFrame:
+	df = pl.DataFrame(raw)
+
+	return df
+
+	...
 
 def store_to_local():
 	
@@ -79,5 +86,7 @@ KEY = "bronze/yahoo/history/dt=2026-02-20/symbol=SOFI/run_id=20260220053445Z.jso
 if __name__ == "__main__":
 	...
 	res = fetch_json_from_bronze(BRONZE_BUCKET_ID, KEY)
-	normalize_yf(res)
+	table = normalize_json(res)
+	df = clean_bronze(table)
+	print(df)
 	
